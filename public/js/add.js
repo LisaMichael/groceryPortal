7b55303e4a8e573715df8cc26f5fbf2e404fbd23
Lisa <lisa.michael@hotmail.com>
2020-01-24 19:41:43 -0500

    got the add page working

--- public/js/add.js
+++ public/js/add.js
@@ -1,34 +1,39 @@
-  // When user clicks add-btn
-  $("#add-btn").on("click", function (event) {
-    event.preventDefault();
+// When user clicks add-btn
+$("#add-btn").on("click", function (event) {
+  event.preventDefault();
+  console.log("Inside new.js");
 
-    // Make a new object
-    var newItem = {
-      item_name: $("#item_name").val().trim(),
-      price: $("#price").val().trim(),
-      quantity: $("#quantity").val().trim(),
-      aisle_number: $("#aisle_number").val().trim()
-    };
+  // Make a new object
+  var newItem = {
+    item_name: $("#item_name").val().trim(),
+    department: $("#department").val().trim(),
+    price: $("#price").val().trim(),
+    quantity: $("#quantity").val().trim(),
+    aisle_number: $("#aisle_number").val().trim()
+  };
+  console.log(newItem);
 
-    // Send an AJAX POST-request with jQuery
-    $.post("/api/posts", newItem)
-      // On success, run the following code
-      .then(function (data) {
-        // Log the data we found
-        console.log(data);
-        newItem
-          .create({
-            item_name: $("#item_name").val().trim(),
-            price: $("#price").val().trim(),
-            quantity: $("#quantity").val().trim(),
-            aisle_number: $("#aisle_number").val().trim()
-          })
-      });
+  // Send an AJAX POST-request with jQuery
+  $.post("/api/posts", newItem)
+    // On success, run the following code
+    .then(function (data) {
+      // Log the data we found
+      console.log(data);
+      // newItem
+      //   .create({
+      //     item_name: $("#item_name").val().trim(),
+      //     price: $("#price").val().trim(),
+      //     quantity: $("#quantity").val().trim(),
+      //     aisle_number: $("#aisle_number").val().trim()
+      //   })
+    });
 
-    // Empty each input box by replacing the value with an empty string
-    $("#item_name").val("");
-    $("#price").val("");
-    $("#quantity").val("");
-    $("#aisle_number").val("");
+  // Empty each input box by replacing the value with an empty string
+  $("#item_name").val("");
+  $("#department").val("");
+  $("#price").val("");
+  $("#quantity").val("");
+  $("#aisle_number").val("");
 
-  });
+  
+});
